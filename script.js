7847fb6a997bce22c872d8eb740469cc698095bc
ad31yaoa <ameymanbekova@mercy.edu>
2025-12-06 18:35:44 -0500

    Add files via upload
    Signed-off-by: ad31yaoa <ameymanbekova@mercy.edu>

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const menuBtn = document.querySelector(".menu");
+  const nav = document.querySelector(".newone");
+
+  menuBtn.addEventListener("click", function() {
+    nav.classList.toggle("show");
+  });
+});
